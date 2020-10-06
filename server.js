107ef202c0f812a2dab4f4a7b2a83a6fc3267f6b
Vincent Hong <vinlegend1@gmail.com>
2020-10-06 10:17:17 +0800

    not working?

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const flash = require('connect-flash');
 const cors = require('cors');
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
+const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
@@ -39,7 +40,5 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
 }
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
