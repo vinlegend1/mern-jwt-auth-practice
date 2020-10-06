61f928288ca146d641637757dba7ce5a1b6d8bf5
Vincent Hong <vinlegend1@gmail.com>
2020-10-06 15:48:32 +0800

    try some fixing

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const flash = require('connect-flash');
 const cors = require('cors');
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
 
 const app = express();
 
@@ -23,7 +20,6 @@ connection.once('open', () => {
 require('./passport');
 app.use(cookieParser());
 app.use(cors());
-app.use(flash());
 app.use(passport.initialize());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -41,4 +37,4 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
